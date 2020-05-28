01487567f81ed88eb24542e84142bcd4e9727284
Daniel Galndo <d.galindo@uniandes.edu.co>
2020-05-28 17:04:20 -0500

    No está muerto lo que puede yacer eternamente y con eones extranios aun morir puede la muerte

--- routes/tiendaTendero.js
+++ routes/tiendaTendero.js
@@ -8,14 +8,65 @@ const client = new Client({
 });
 client.connect();
 
-router.get('/', function (req, res, next) {
-    client.query('SELECT * FROM pedido_pedido', function (err, result) {
-        if (err) {
+router.get('/:id/aiuda/:nombre',function(req, res, next){
+    client.query('UPDATE tienda_tienda SET duenio = \'' +req.params.nombre + '\' WHERE id = ' + req.params.id, function(err,result){
+        if(err){
             console.log(err);
             res.status(400).send(err);
         }
         res.status(200).send(result.rows);
     });
 });
+
+
+router.get('/', function (req, res, next) {
+   client.query('SELECT * FROM tienda_tienda', function (err, result) {
+       if (err) {
+           console.log(err);
+           res.status(400).send(err);
+       }
+       res.status(200).send(result.rows);
+   });
+});
+
+router.get('/:id', function (req, res, next) {
+    client.query('SELECT id FROM tienda_tienda WHERE duenio = \'' + req.params.id + '\'', function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    });
+ });
+
+ router.get('/info/:id', function (req, res, next) {
+    client.query('SELECT duenio FROM tienda_tienda WHERE id = ' + req.params.id, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    });
+ });
+
+ router.delete('/:id', function (req, res) {
+    client.query('UPDATE tienda_tienda SET duenio = NULL WHERE id = ' + req.params.id, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    });
+ });
+
+//router.get('/', function (req, res, next) {
+//    client.query('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' ORDER BY table_name', function (err, result) {
+//        if (err) {
+//            console.log(err);
+//           res.status(400).send(err);
+//        }
+//        res.status(200).send(result.rows);
+//   });
+//});
 
 module.exports = router;
